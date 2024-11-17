71bcfcd64b1a39ce20487f318ed5431520966c3d
Josh Day <emailjoshday@gmail.com>
2024-11-17 13:59:07 -0500

    working!  not sure how to write unit tests for REPL behavior though

--- src/Nest.jl
+++ src/Nest.jl
@@ -1,5 +1,36 @@
 module Nest
 
-# Write your package code here.
+export Indexer
+
+#-----------------------------------------------------------------------------# Indexer
+struct Indexer{T, K, V, P <: NamedTuple} <: AbstractDict{K, V}
+    parent::T
+    index_children::Dict{K, V}
+    prop_children::P
+end
+function Indexer(x::T) where {T}
+    index(x) = x isa T ? Indexer(x) : x
+    Indexer(x,
+        Dict(k => getindex(x, k) for k in keys(x)),
+        NamedTuple(k => index(getproperty(x, k)) for k in propertynames(x))
+    )
+end
+Base.summary(io::IO, idx::Indexer) = (print(io, "Indexer: "); summary(io, parent(idx)))
+
+Base.iterate(idx::Indexer) = iterate(index_children(idx))
+Base.iterate(idx::Indexer, state) = iterate(index_children(idx), state)
+Base.length(idx::Indexer) = length(index_children(idx))
+
+parent(idx::Indexer) = getfield(idx, :parent)
+index_children(idx::Indexer) = getfield(idx, :index_children)
+prop_children(idx::Indexer) = getfield(idx, :prop_children)
+
+Base.getindex(idx::Indexer, key) = index_children(idx)[key]
+Base.keys(idx::Indexer) = keys(index_children(idx))
+Base.values(idx::Indexer) = values(index_children(idx))
+
+Base.getproperty(idx::Indexer, key::Symbol) = prop_children(idx)[key]
+Base.propertynames(idx::Indexer) = propertynames(prop_children(idx))
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
 using Nest
+
+using REPL.REPLCompletions
 using Test
 
+# completions(x) = Symbol.(REPLCompletions.completion_text.(REPLCompletions.completions(x, length(x))[1]))
+completion_symbols(x) = Symbol.(completion_text.(completions(x, length(x))[1]))
+
+
 @testset "Nest.jl" begin
-    # Write your tests here.
+    @testset "NamedTuple" begin
+        nt = (; a = (; b = (; c = (; d = 1))), b=(; c = 2))
+        idx = Indexer(nt)
+        @test completion_symbols("idx.") == [:a, :b]
+        @test completion_symbols("idx.a.") == [:b]
+        @test completion_symbols("idx.a.b.") == [:c]
+        @test completion_symbols("idx.a.b.c.") == [:d]
+        @test idx.a.b.c.d == 1
+    end
 end
